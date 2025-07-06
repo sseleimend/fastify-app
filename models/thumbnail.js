d5f40f2042d184339245207f02928d9dbeee9ad3
Victor Seleimend <sseleimend@gmail.com>
2025-07-06 12:07:11 -0300

    Add thumbnail model with schema for video thumbnails

---
+++ models/thumbnail.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const thumbnailSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    videoName: {
+      type: String,
+      required: true,
+    },
+    version: {
+      type: String,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    paid: {
+      type: String,
+      default: false,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export default mongoose.model("Thumbnail", thumbnailSchema);
